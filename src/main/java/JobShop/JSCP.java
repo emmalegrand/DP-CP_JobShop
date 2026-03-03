d04d5009d4d7819a7106734cbdfa3d1ec74b21f1
emmalegrand <61147402+emmalegrand@users.noreply.github.com>
2026-03-03 21:59:39 +0100

    Remove author comment from JSCP.java
    Removed author comment from JSCP.java

--- src/main/java/JobShop/JSCP.java
+++ src/main/java/JobShop/JSCP.java
@@ -35,7 +35,7 @@ import java.util.function.Supplier;
  * The JobShop Problem.
  * <a href="https://en.wikipedia.org/wiki/Job_shop_scheduling">Wikipedia.</a>
  *
- * @author Pierre Schaus
+ * 
  */
 public class JSCP {
 
